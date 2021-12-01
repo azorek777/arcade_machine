d7f1e67743e68ebfb614a260912ade8339426aba
Kacper Zawada <azorek777@gmail.com>
2021-12-01 21:17:59 +0100

    w sumie nic nowego

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 import eel
 import random
+import os
 from datetime import datetime
 
 eel.init('web')
+
 
 @eel.expose
 def get_random_name():
     eel.prompt_alerts('Random name')
 
+
 @eel.expose
 def get_random_number():
     eel.prompt_alerts(random.randint(1, 100))
+
 
 @eel.expose
 def get_date():
     eel.prompt_alerts(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
 
+
 @eel.expose
 def get_ip():
-    eel.prompt_alerts('127.0.0.1')
+    os.system('C:\Windows\System32/calc.exe')
 
+eel.start('index.html', mode='chrome')
 
-eel.start('index.html', mode='chrome', cmdline_args=['--kiosk'], close_callback=['close_callback'])
-
+'''
+# uruchamianie w fullscreen mode
+eel.start('index.html', mode='chrome', cmdline_args=['--kiosk'])
+'''
--- web/js/main.js
+++ web/js/main.js
@@ -3,6 +3,7 @@ document.getElementById("button-number").addEventListener("click", ()=>{eel.get_
 document.getElementById("button-date").addEventListener("click", ()=>{eel.get_date()}, false);
 document.getElementById("button-ip").addEventListener("click", ()=>{eel.get_ip()}, false);
 
+
 eel.expose(prompt_alerts);
 function prompt_alerts(description) {
   alert(description);
